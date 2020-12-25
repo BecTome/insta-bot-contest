93fc12666a9fb7bc40af3fdfcc080c66e1c463b1
BecTome <68814627+BecTome@users.noreply.github.com>
2020-12-25 21:55:42 +0100

    Login click

--- instabot.py
+++ instabot.py
@@ -5,9 +5,19 @@ from selenium import webdriver
 # # %% Open the browser and enter instagram
 # Open browser
 browser = webdriver.Firefox(executable_path='./venv/bin/geckodriver')
-# Get HTTP request
-browser.get('https://www.instagram.com/')
-# Wait 5 seconds
-sleep(5)
-# Close it
-browser.close()
+
+# If Selenium can’t find an element, then it waits for five seconds to allow 
+# everything to load and tries again. 
+browser.implicitly_wait(5)
+
+browser.get('https://www.instagram.com/') # Get HTTP request
+
+# finds the element <a> whose text is equal to Log in. It does this using XPath,
+# but there are a few other methods you could use.
+login_link = browser.find_element_by_xpath("//a[text()='Log in']")
+
+login_link.click() # clicks on the found element <a> for the login link.
+
+sleep(5) # Wait 5 seconds
+
+browser.close() # Close it
